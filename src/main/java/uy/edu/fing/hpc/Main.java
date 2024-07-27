d1917577e454b358746d4732f4d3d6bc1522b682
Victoria Rincon <vito.rincon@gmail.com>
2024-07-27 03:13:50 -0300

    Adds parallelism and synchronization

--- src/main/java/uy/edu/fing/hpc/Main.java
+++ src/main/java/uy/edu/fing/hpc/Main.java
@@ -1,37 +1,133 @@
 package uy.edu.fing.hpc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
+
 public class Main {
-    private static final String CONTAINERS_PATH = "data/containers.csv";
-    private static final String OSM_PATH = "data/uruguay-latest.osm.pbf";
+	private static final String CONTAINERS_PATH = "data/containers.csv";
+	private static final String OSM_PATH = "data/uruguay-latest.osm.pbf";
+	private static final int NUM_THREADS = 4; // Número de hilos
+	private static final double TEMPERATURE = 100.0; // Initial temperature
+	private static final double TEMPERATURE_DECAY = 0.95; // Temperature decay factor
 
-    public static void main(String[] args) {
-        var dataSource = new DataSource(CONTAINERS_PATH);
-        dataSource.load();
+	public static void main(String[] args) {
+		try {
+			var dataSource = new DataSource(CONTAINERS_PATH);
+			dataSource.load();
+			double temperature = TEMPERATURE;
+			var containers = dataSource.getContainers();
+			var circuits = dataSource.getCircuits();
 
-        var containers = dataSource.getContainers();
-        var circuits = dataSource.getCircuits();
+			Router.getInstance().init(containers, OSM_PATH);
 
-        Router.getInstance().init(containers, OSM_PATH);
+			var solution = new Solution(circuits);
 
-        var solution = new Solution(circuits);
-        var cost = solution.computeCost();
+			Long cost = solution.computeCost();
 
-        System.out.println("Costo inicial: " + cost);
+			System.out.println("Costo inicial: " + cost);
 
-        for (var i = 0; i < 1000; i++) {
-            var neighbor = solution.getNeighbor();
-            var neighborCost = neighbor.computeCost();
+			// Estructura concurrente para almacenar los vecinos recorridos
+			ConcurrentHashMap<Solution, Long> visitedNeighbors = new ConcurrentHashMap<>();
+			Long bestFinalCost = cost;
+			
 
-            if (neighborCost < cost) {
-                solution = neighbor;
-                cost = neighborCost;
+			// Semáforo para controlar el acceso a la variable cost y al hash
+			Semaphore semaphore = new Semaphore(1);
+			Semaphore semaphoreBest = new Semaphore(1);
 
-                System.out.println("Nuevo costo: " + neighborCost + " en iteración " + i);
-            }
-        }
+			// Crear un pool de hilos
+			ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
 
-        System.out.println("Costo final: " + cost);
+			// Crear tareas para paralelizar las iteraciones
+			int totalIterations = 2000;
+			int iterationsPerThread = totalIterations / NUM_THREADS;
 
-        System.out.println("Listo");
-    }
+			while (temperature > 90) {
+				List<Future<Solution>> futures = new ArrayList<>();
+				for (int i = 0; i < NUM_THREADS; i++) {
+					int startIteration = i * iterationsPerThread;
+					int endIteration = (i + 1) * iterationsPerThread;
+					futures.add(executorService.submit(
+							new Worker7(solution, startIteration, endIteration, visitedNeighbors, semaphore, cost)));
+				}
+				
+				// Esperar a que todas las tareas terminen
+				for (Future<Solution> future : futures) {
+					
+					solution = future.get();
+					cost = visitedNeighbors.get(solution);
+					
+					semaphoreBest.acquire();
+					if(bestFinalCost > cost) {
+						bestFinalCost = cost;
+					}
+					semaphoreBest.release();
+				}
+
+				temperature *= TEMPERATURE_DECAY;
+			}
+
+			executorService.shutdown();
+
+			System.out.println("Costo final: " + bestFinalCost);
+			System.out.println("Listo");
+			
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class Worker implements Callable<Solution> {
+	private Solution solution;
+	private int startIteration;
+	private int endIteration;
+	private ConcurrentHashMap<Solution, Long> visitedNeighbors;
+	private Semaphore semaphore;
+	private Long cost;
+
+	public Worker(Solution solution, int startIteration, int endIteration,
+			ConcurrentHashMap<Solution, Long> visitedNeighbors, Semaphore semaphore, long cost) {
+		this.solution = solution;
+		this.startIteration = startIteration;
+		this.endIteration = endIteration;
+		this.visitedNeighbors = visitedNeighbors;
+		this.semaphore = semaphore;
+		this.cost = cost;
+	}
+
+	@Override
+	public Solution call() {
+		Solution bestSolution = solution;
+		long bestCost = cost;
+
+		int i = startIteration;
+
+		while (i < endIteration) { //Se puede agregar condicion de que si llega a una solucion suficientemente chica deje de iterar
+			Solution neighbor = bestSolution.getNeighbor();
+			if (!visitedNeighbors.containsKey(neighbor)) {
+				long neighborCost = neighbor.computeCost();
+				try {
+					semaphore.acquire();
+					visitedNeighbors.put(neighbor, neighborCost);
+					if (neighborCost < bestCost) {
+						bestSolution = neighbor;
+						bestCost = neighborCost;
+						cost = bestCost;
+						System.out.println("Nuevo costo: " + neighborCost + " en iteración " + i);
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaphore.release();
+					i++;
+				}
+			}
+		}
+
+		return bestSolution;
+	}
 }
